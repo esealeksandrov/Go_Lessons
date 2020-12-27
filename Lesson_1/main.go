a87a58df6528f220d2bbf1da704a675b0039d698
Aleksandrov Evgeniy <soil3345@gmail.com>
2020-12-28 02:25:30 +0300

    all Error Fixed
    add function now() -> return current time:string

--- Lesson_1/main.go
+++ Lesson_1/main.go
@@ -1,8 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"time"
+)
 
 
 func main(){
+	var ntp_server string = "0.beevik-ntp.pool.ntp.org"
+	fmt.Printf("Now is: %s\n", now(ntp_server))
+}
 
-	print("dfsdfsdf"+ Пример1)
+func now(host string) string {
+	for {
+		c_time, err := ntp.Time(host)
+		if err != nil{
+			fmt.Printf("Wait ! Error %e\n", err.Error())
+			time.Sleep(1 * time.Second)
+		} else{
+			return c_time.Format("2006-January-02 15:04:05")
+		}
+	}
 }
